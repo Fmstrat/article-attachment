42582254f37fb76b80d6501dd6e52faaddc63dc4
root <root@northwyck>
2019-01-31 16:29:45 -0500

    Initial add.

---
+++ init.php
@@ -0,0 +1,42 @@
+<?php
+class Article_Attachment extends Plugin {
+
+	private $host;
+
+	function about() {
+		return array(1.0,
+			"If exists, put the largest media attachment at the top of feed content.",
+			"fmstrat");
+	}
+
+	function init($host) {
+		$this->host = $host;
+		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+	}
+
+	function hook_article_filter($article) {
+		//if (strpos($article["link"], "deviantart.com/art/") !== FALSE) {
+			//ob_start();
+			//var_dump($article);
+			//$result = ob_get_clean();
+			//$article['content'] = "<pre>".$result."</pre><br><br>".$article["content"];
+			$res = $this->pdo->query("select content_url
+							from ttrss_enclosures
+							where post_id=(select id
+									from ttrss_entries
+									where guid='".$article['guid_hashed']."')
+							order by width desc
+							limit 1;");
+			while ($line = $res->fetch()) {
+				$article['content'] = "<img src='".$line["content_url"]."'><br><br>".$article["content"];
+			}
+		//}
+		return $article;
+	}
+
+	function api_version() {
+		return 2;
+	}
+
+}
+?>
